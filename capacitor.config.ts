2ac83e98d3792b6c9be7b1f1d92064a752f33295
LeonanUCM <leonan@gmail.com>
2024-11-07 19:32:09 +0100

    pinch zoom android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Frutos2_0',
-  webDir: 'www'
+  webDir: 'www',
+    android: {
+    allowMixedContent: true,
+    webContentsDebuggingEnabled: true,
+    zoomEnabled: true
+  }
 };
 
 export default config;
